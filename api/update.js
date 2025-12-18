6c77c260fad4d9b9a6d4fdc3196c92532939871d
NOVALITELTD <166810724+NOVALITELTD@users.noreply.github.com>
2025-12-18 02:30:52 +0100

    Update update.js

--- api/update.js
+++ api/update.js
@@ -1,3 +1,8 @@
 module.exports = (req, res) => {
+  if (req.method === "POST") {
+    console.log(req.body);
+    return res.status(200).json({ status: "ok", success: true });
+  }
+
   res.status(200).send("OK");
 };
